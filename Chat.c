77be193355497d159028f40380d3b6c9ecf9e56b
jeonghokim <jeonghokim@cs.stonybrook.edu>
2016-04-23 17:10:54 -0400

    chat

--- Chat.c
+++ Chat.c
@@ -3,20 +3,43 @@
 int main(int argc, char** argv) {
 	
 	int unixfd = atoi(argv[1]);
+	int msgflag = FALSE;
 	char buf[MAX_LEN];
+
+	fd_set readSet;
+	fd_set readySet;
 
 	memset(buf, 0, MAX_LEN);
   
+	FD_ZERO(&readSet);
+	FD_SET(STDIN, &readSet);
+	FD_SET(unixfd, &readSet);
+
 	while(TRUE) {
-		fgets(buf, MAX_LEN, stdin);
-		buf[strlen(buf)-1] = '\0';
+		readySet = readSet;
+		select(unixfd+1, &readySet, NULL, NULL, NULL);
 
-		if(strcmp(buf, "close") == 0) {
-			break;
+		if(FD_ISSET(STDIN, &readySet)) {
+			fgets(buf, MAX_LEN, stdin);
+			buf[strlen(buf)-1] = '\0';
+
+			if(strcmp(buf, "/close") == 0) {
+				break;
+			}
+			send(unixfd, buf, sizeof(buf), 0);
+			msgflag = TRUE;
 		}
-
-		send(unixfd, buf, sizeof(buf), 0);
-		printf("%s\n", buf);
+		if(FD_ISSET(unixfd, &readySet)) {
+			memset(buf, 0, MAX_LEN);
+			recv(unixfd, buf, MAX_LEN, 0);
+			if(msgflag) {
+				printf("> ");
+			} else {
+				printf("< ");
+			}
+			printf("%s\n", buf);
+			msgflag = FALSE;
+		}
 	}
 
 	return 0;
